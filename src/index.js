44f03bfa5fe480aa5f724dd01e62069ed9724538
Fanttatraieste <118627923+Fanttatraieste@users.noreply.github.com>
2023-05-21 18:04:46 +0300

    xfgfshg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch("https://restcountries.com/v3.1/all?limit=20")
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data);
+    });
